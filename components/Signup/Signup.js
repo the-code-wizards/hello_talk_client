77f87e99eebdbe853ce93d57ec4ca4795ca22cbb
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-11 08:42:43 +0600

    signup

--- components/Signup/Signup.js
+++ components/Signup/Signup.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HiArrowLeft } from 'react-icons/fa';
+import CustomBtn from '../reusable/CustomBtn';
 
 const Signup = () => {
     return (
         <div>
-            
+            <div className='flex justify-around'>
+                <HiArrowLeft />
+                <h2>Create Your Profile</h2>
+                <CustomBtn>Login</CustomBtn>
+            </div>
         </div>
     );
 };
---
+++ pages/signup/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Signup from '../../components/Signup/Signup';
+
+const index = () => {
+    return (
+        <div>
+            <Signup/>
+        </div>
+    );
+};
+
+export default index;
