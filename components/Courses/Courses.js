d0064131ead9438f8b28302c9656c02adbceaaa1
unknown <mosharafctg111@gmail.com>
2023-01-12 01:07:30 +0600

    added the latest card

--- components/Courses/Courses.js
+++ components/Courses/Courses.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import SingleCard from './SingleCard';
 
 
@@ -26,14 +27,30 @@ const Courses = () => {
 
         },
     ]
+
+    // const {data: courses = []} = useQuery({
+    //     queryKey: ["courses"],
+    //     queryFn: async()=>{
+    //         const res = await fetch('https://hello-talk-server.vercel.app/courses') 
+    //         const data = await res.json()
+    //         return data;
+    //     }
+    // })
+    const [courses, setCourses] = useState([])
+    useEffect(()=>{
+        fetch('https://hello-talk-webserver.vercel.app/courses')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
+
     return (
    
-          <div className='max-w-[1240px] mx-auto p-20 bg-[#1A5398]'>
+          <div className='max-w-[1240px] mx-auto p-20'>
   
            <div className='grid grid-cols-3 gap-10'>
            {
-                AllPackage.map(packages => <SingleCard
-                key={packages.id}
+                courses.map(packages => <SingleCard
+                key={packages._id}
                 packages={packages}
                 ></SingleCard> )
             }
--- components/Courses/SingleCard.js
+++ components/Courses/SingleCard.js
@@ -3,22 +3,27 @@ import React from 'react';
 
 
 const SingleCard = ({packages}) => {
-    const {id, title, picture , details} = packages;
+    const {_id, title, picture , details, offer_price, price} = packages;
     return (
-        <div className="card card-compact bg-white shadow-2xl">
-            <div className='p-3'>
-            <img className='rounded-xl' src={picture} alt="Shoes" />
+
+         <div className="card card-compact bg-white shadow-2xl">
+            <div className='p-3  mx-auto'>
+            <img className='rounded-xl h-44' src={picture} alt="Shoes" />
             </div>
   <div className="card-body">
-    <h2 className="card-title text-2xl text-[#00CC17] font-bold">{title}</h2>
-    <p>{details}</p>
+    <h2 className="card-title text-2xl text-[#00CC17] font-bold uppercase">{title}</h2>
+    <p>{details.slice(0,90)}...</p>
   </div>
-    <div className="card-actions justify-end">
-      <button className="btn bg-[#00CC17] border-none w-full rounded-t-none ">Buy Now</button>
-   
-
+    <div className="card-actions justify-between flex p-3 items-center">
+      <div>
+      <p className='text-xl text-gray-300'><del>৳{price}</del></p>
+      <h2 className='text-2xl text-[#00CC17] font-bold'>	৳{offer_price}</h2>
+      </div>
+      <button className="btn bg-[#00CC17] border-none">Details</button>
     </div>
 </div>
+  
+      
     );
 };
 
