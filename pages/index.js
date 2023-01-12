b3a7a4cd85a0d8bccbb452555a729ca5e684fe70
al-shaimon <alshaimon968@gmail.com>
2023-01-12 13:25:21 +0600

    blog page initial design ready image issue

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Package from '../components/packages/Package'
-import Blog from '../components/Blog/Blog'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+import Package from '../components/packages/Package';
+import Blog from '../components/Blog/Blog';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -21,8 +21,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Package/>
+      <Blog></Blog>
     </>
-  )
+  );
 }
