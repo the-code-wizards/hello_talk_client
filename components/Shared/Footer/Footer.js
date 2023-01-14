dfa2972ce02343511150353fdbc5bfd6b6586c6a
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-14 19:55:46 +0600

    nav edited

--- components/Shared/Footer/Footer.js
+++ components/Shared/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
         <div style={{ backgroundImage: `url("back.svg")` }}>
             <div className='max-w-[1240px] mx-auto'>
                 <div className='flex flex-col lg:flex-row justify-center items-center py-20'>
-                    <h1 className='font-featherBold text-white text-xl  lg:md:text-3xl'>Learn a language with Hello Talk.</h1>
-                    <button className="mt-[15px] bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] w-[60%] lg:md:mx-0 mx-auto lg:md:w-[15%] hover:bg-[#61E002] ml-5">Get Started</button>
+                    <h1 className='font-featherBold text-white text-xl lg:md:mr-4 lg:md:text-3xl'>Learn a language with Hello Talk.</h1>
+                    <button className="mt-[15px] bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] w-[60%] lg:md:mx-0 mx-auto lg:md:w-[15%] hover:bg-[#171d13] ml-5">Get Started</button>
                 </div>
                 <footer className="footer p-10 text-neutral-content "  >
                     <div>
--- components/Shared/Navbar/Navbar.js
+++ components/Shared/Navbar/Navbar.js
@@ -1,34 +1,47 @@
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+        localStorage.removeItem('accessToken');
+    };
     return (
-        <nav className='bg-[#194881] relative z-10 '>
-            <div className="navbar mx-auto fixed bg-[#194881] ">
+        <nav className='relative z-10'>
+            <div className="lg:md:px-10 px-0 shadow-xl navbar mx-auto fixed bg-gradient-from-l bg-gradient-to-l from-[#194881] to-[rgb(53,106,172)]">
                 <div className="navbar-start ">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="font-featherBold menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Blog</a></li>
-                            <li><a>Courses</a></li>
-                            <li><a>Learn</a></li>
-                            <li><a>Contact</a></li>
+                        <ul tabIndex={0} className="font-featherBold menu menu-compact dropdown-content mt-3 p-2 shadow bg-green-300 rounded-box w-52  font-medium">
+                            <li><Link href='/blogs'>Blog</Link></li>
+                            <li><Link href='/courses'>Courses</Link></li>
+                            <li><Link href='/blogs'>Learn</Link></li>
+                            <li><Link href='/contact'>Contact Us</Link></li>
                         </ul>
                     </div>
-                    <Link href="/" className="flex text-2xl w-[40px] items-center"><img src="Logo2.png" alt="/" /><a className="btn btn-ghost normal-case text-xl lg:text-3xl text-white font-featherBold ml-[-10px]">Hello Talk</a></Link>
+                    <Link href="/" className="flex text-2xl w-[50px] items-center"><img src="Logo2.png" alt="/" /><p className="ml-2 normal-case text-xl lg:text-2xl text-white font-featherBold">HelloTalk</p></Link>
                 </div>
-                <div className="navbar-center hidden lg:flex ">
-                    <ul className="font-featherBold menu menu-horizontal px-1 uppercase text-white">
-                        <li><a><Link href='/blogs'>Blog</Link></a></li>
-                        <li><a><Link href='/courses'>Courses</Link></a></li>
-                        <li><a>Learn</a></li>
-                        <li><a>Contact</a></li>
+                <div className="navbar-end hidden lg:flex ">
+                    <ul className="font-featherBold menu menu-horizontal px-1 text-white font-medium">
+                        <li><Link href='/blogs'>Blog</Link></li>
+                            <li><Link href='/courses'>Courses</Link></li>
+                            <li><Link href='/blogs'>Learn</Link></li>
+                            <li><Link href='/contact'>Contact Us</Link></li>
+                        {user ? 
+                             <li> <button className=" bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[8px] px-5 rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] hover:bg-[#61E002]" onClick={logout}>Log Out</button>
+                            </li>                           
+                            :
+                            <li> <Link className=" bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[8px] px-5 rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] hover:bg-[#61E002]" href="/signin">Log In</Link>
+                            </li>
+                        }
                     </ul>
-                </div>
-                <div className="navbar-end pr-5">
-                    <Link className="mt-[15px] bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[8px] px-5 rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] hover:bg-[#61E002]" href="/signin/"> SignIn</Link>
                 </div>
             </div>
 
