1d0a05354dfd8ee0f8f558080d1b94243f8aeb87
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-14 21:56:12 +0600

    learn page and navbar edited

--- components/Learn/Sidebar.js
+++ components/Learn/Sidebar.js
@@ -8,16 +8,16 @@ import { BsStack, BsPeople } from 'react-icons/bs';
 const Sidebar = () => {
     const router = useRouter()
     return (
-        <div className="drawer drawer-mobile m-4">
+        <div className="drawer drawer-mobile m-4 sticky">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 {/* <!-- Page content here --> */}
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side lg:md:w-[55%] h-[50%] rounded-2xl shadow-xl">
+            <div className="drawer-side lg:md:w-[60%] h-[50%] rounded-2xl shadow-xl">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-[#dbf8df] text-[#504f4f] font-bold border-[1px] ">
+                <ul className="menu lg:md:p-4 w-80 bg-[#dbf8df] text-[#504f4f] font-bold border-[1px] ">
                     {/* <!-- Sidebar content here --> */}
                     <li><button className="" type="button" onClick={() => router.push('/learn')}><GiNotebook className="text-xl text-red-400 mr-[5px]" />Learn</button></li>
                     <li><button type="button" onClick={() => router.push('/shop')}><AiOutlineShoppingCart className="text-xl text-red-400 mr-[5px]" />Shop</button></li>
