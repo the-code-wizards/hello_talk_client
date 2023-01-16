0a41fbe088c40f0782874a07efc534a9f1ccaae4
al-shaimon <alshaimon968@gmail.com>
2023-01-16 23:22:05 +0600

    flashcards

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import HomePage from '../components/Home/HomePage'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import HomePage from '../components/Home/HomePage';
 import Flashcards from '../components/Flashcards/Flashcards';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-
   return (
     <>
       <Head>
@@ -15,8 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <HomePage /> */}
-      <Flashcards/>
+      <HomePage />
     </>
   );
 }
