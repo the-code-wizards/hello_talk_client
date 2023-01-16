766b12c0ac928484918d5d876d89d856e21e66eb
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-16 23:04:55 +0600

    level update

--- pages/level/[level].js
+++ pages/level/[level].js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useRouter } from 'next/router'
 import Navbar from '../../components/Shared/Navbar/Navbar'
 
 const Level = () => {
+     const router = useRouter()
+  const { id } = router.query
+    console.log(id)
+
     return (
         <div>
             <Navbar/>
