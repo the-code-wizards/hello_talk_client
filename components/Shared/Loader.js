952aa82743d61ee35c24edd66cd391822bbc8d84
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-19 01:37:06 +0600

    mergrd

--- components/Shared/Loader.js
+++ components/Shared/Loader.js
@@ -5,9 +5,9 @@ import loader from "../../resourses/lottieJson/loader.json";
 const Loader = () => {
     return (
         <div>
-             <div className="w-[200px] h-[200px]">
-                                <Lottie animationData={loader} loop={true} />
-                           </div>
+            <div className="w-[200px] h-[200px]">
+                <Lottie animationData={loader} loop={true} />
+            </div>
         </div>
     );
 };
