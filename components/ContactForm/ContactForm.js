32a1c7bbf8f95c795484daf2d38e10d406ffb1f6
unknown <mosharafctg111@gmail.com>
2023-01-18 16:44:26 +0600

    added the some corection

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -11,25 +11,25 @@ const ContactForm = () => {
         <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 text-gray-800 py-32">
         <div className="flex flex-col justify-between">
             <div className="space-y-2">
-                <h2 className="text-3xl font-bold leading-tight lg:text-5xl">Lets Contact <br /> with Us!</h2>
-                <div className="text-gray-600">Full free to Message</div>
+                <h2 className="text-3xl font-bold text-[#00E019] leading-tight lg:text-5xl">Contact Us</h2>
+                <div className="text-gray-600">Feel free to send us any feedback</div>
             </div>
             <div className='w-full'>
             <Lottie animationData={contactGif} loop={true}></Lottie>
             </div>
         </div>
-        <form novalidate="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+        <form action="https://formsubmit.co/your@email.com" method="POST" className="space-y-6 ng-untouched ng-pristine ng-valid">
             <div>
                 <label for="name" className="text-sm">Full name</label>
-                <input id="name" type="text" placeholder="" className="w-full p-3 rounded bg-gray-100" />
+                <input id="name" name='name' type="text" placeholder="" className="w-full p-3 rounded bg-gray-100" />
             </div>
             <div>
                 <label for="email" className="text-sm">Email</label>
-                <input id="email" type="email" className="w-full p-3 rounded bg-gray-100" />
+                <input id="email" name='email' type="email" className="w-full p-3 rounded bg-gray-100" />
             </div>
             <div>
                 <label for="message" className="text-sm">Message</label>
-                <textarea id="message" rows="3" className="w-full p-3 rounded bg-gray-100" spellcheck="false"></textarea>
+                <textarea id="message" name='message' rows="3" className="w-full p-3 rounded bg-gray-100" spellcheck="false"></textarea>
             </div>
             <div className='flex justify-center'>
             <button type="submit" className="mt-[15px] bg-[#58cc02] border-[#61B800] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] rounded-xl text-white font-bold text-[14px] focus:border-b-[2px] w-full  hover:bg-[#61E002]">Send Message</button>
