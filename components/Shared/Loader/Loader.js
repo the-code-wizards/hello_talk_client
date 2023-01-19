90cf6d256f84037d4be486a39667ae151bc9c80c
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-01-19 18:51:30 +0600

    added  modal in the blog page and finalize blog page

--- components/Shared/Loader/Loader.js
+++ components/Shared/Loader/Loader.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import Lottie from "lottie-react";
 import loader from "../../../resourses/lottieJson/loader.json";
 
 const Loader = () => {
-    return (
-        <div>
-            <div className="w-[200px] h-[200px]">
-                <Lottie animationData={loader} loop={true} />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="w-[200px] h-[200px]">
+        <Lottie animationData={loader} loop={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
