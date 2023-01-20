fdc0f4e1b40c6df1408a3a277433848b854317f9
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-20 16:20:12 +0600

    signup lottie

--- components/Signup/Signup.js
+++ components/Signup/Signup.js
@@ -9,7 +9,8 @@ import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfil
 import { AiOutlineGoogle } from 'react-icons/ai';
 import { TfiFacebook } from 'react-icons/tfi';
 import { useRouter } from 'next/router'
-// import { useNavigate } from 'react-router-dom';
+import Lottie from "lottie-react";
+import signup from '../../resources/lottieJson/signup.json'
 
 const Signup = () => {    
     const router = useRouter()
@@ -81,7 +82,7 @@ const Signup = () => {
         .then(data => {
             console.log(data)
             if(data.acknowledged){
-                alert("Account created")
+                swal.success("Congratulations! Account created successfully")
             }
         })
         await createUserWithEmailAndPassword(data.email, data.password, data?.age);
@@ -93,68 +94,75 @@ const Signup = () => {
          <Head>
         <title>Hello Talk - Sign Up</title>
       </Head>
-            <div className='p-[30px] '>
-            <div className='flex justify-between items-center'>
+            <div className='p-[30px] max-h-screen'>
+            <div className='flex justify-between items-center '>
                 <Link href="/"><HiArrowLeft className="text-2xl cursor-pointer" /></Link>
                 
                     <button type="button" onClick={() => router.push('/signin')} className='bg-[#fff] border-[#CECECE] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[8px] px-[18px] rounded-xl text-[#1cb0f6] font-bold text-[14px] focus:border-b-[2px] hover:bg-[#ece7e7]'>
                    LOGIN
                 </button>
             </div>
-                <h2 className='lg:md:text-2xl text-lg text-center lg:md:mt-4 mt-10 text-[#3C3C3C] font-featherBold'>Create Your Profile</h2>
-                <div className="mt-[25px] min-h-screen my-auto">
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
-                    <input type="number"
-                        placeholder="Age"
-                        className="input w-full max-w-sm bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                        {...register("age", {
-                            required: {
-                                value: true,
-                                message: 'Age is required'
-                            }
-                        })} />
-                    <input type="text" 
-                        placeholder="Name"
-                        className="input w-full max-w-sm bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                        {...register("name", {
-                            required: {
-                                value: true,
-                                message: 'Name is required'
-                            }
-                        })} />
-                    <input type="email"
-                        placeholder="Email"
-                        className="input w-full max-w-sm bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                        {...register("email", {
-                            required: {
-                                value: true,
-                                message: 'Email is required'
-                            }
-                        })} />
-                         <input type="password" 
-                        placeholder="Password"
-                        className="input w-full max-w-sm bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                        {...register("password", {
-                            required: {
-                                value: true,
-                                message: 'Password is required'
-                            }
-                        })}/>
-
-                    <button className="mt-[15px] bg-[#1FC2FF] border-[#1AA8EB] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] lg:md:w-[30%] w-[100%] rounded-xl text-[#fff] font-bold lg:md:text-[15px] text-[12px] focus:border-b-[2px]" type="submit">CREATE ACCOUNT</button>                    
-                      <div className="text-center font-bold text-lg my-[20px]">OR</div>
-                </form>             
-
-                    <div className="flex justify-center gap-x-[5px]">
-                        <button  
-                         onClick={() => signInWithGoogle()}
-                            className="justify-center flex items-center mt-[15px] bg-[#fff] border-[#CECECE] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] rounded-xl text-[#1cb0f6] font-bold text-[14px] focus:border-b-[2px] lg:md:w-[15%] w-[50%] hover:bg-[#E5E5E5]" type="submit"><AiOutlineGoogle className="text-red-400 text-[25px] mr-[4px]"/>Google</button>
+                <div className="grid grid-cols-[600px_minmax(600px,1fr)] gap-x-[20px] ">
+                    <div className="w-[100%] h-[150px]">
+                        <Lottie animationData={signup} loop={true} />
                     </div>
-                    <div className="mt-4 text-center">
-                        <Link href="/terms" className="cursor-pointer font-bold text-center">Terms and condition</Link>
-                        <Link href="/privacy" className="cursor-pointer font-bold text-center ml-3">Privacy policy</Link>
+                    <div>
+                        {/* <h2 className='lg:md:text-2xl text-lg text-center lg:md:mt-4 mt-10 text-[#3C3C3C] font-featherBold'>Create Your Profile</h2> */}
+                        <div className="mt-[25px] my-auto w-full">
+                            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
+                                <input type="number"
+                                    placeholder="Age"
+                                    className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
+                                    {...register("age", {
+                                        required: {
+                                            value: true,
+                                            message: 'Age is required'
+                                        }
+                                    })} />
+                                <input type="text"
+                                    placeholder="Name"
+                                    className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
+                                    {...register("name", {
+                                        required: {
+                                            value: true,
+                                            message: 'Name is required'
+                                        }
+                                    })} />
+                                <input type="email"
+                                    placeholder="Email"
+                                    className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is required'
+                                        }
+                                    })} />
+                                <input type="password"
+                                    placeholder="Password"
+                                    className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: 'Password is required'
+                                        }
+                                    })} />
+
+                                <button className="mt-[15px] bg-[#1FC2FF] border-[#1AA8EB] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] lg:md:w-[40%] w-[100%] rounded-xl text-[#fff] font-bold lg:md:text-[15px] text-[12px] focus:border-b-[2px]" type="submit">CREATE ACCOUNT</button>
+                                <div className="text-center font-bold text-lg my-[20px]">OR</div>
+                            </form>
+
+                            <div className="flex justify-center gap-x-[5px]">
+                                <button
+                                    onClick={() => signInWithGoogle()}
+                                    className="justify-center flex items-center mt-[15px] bg-[#fff] border-[#CECECE] border-t-[2px] border-b-[5px] border-l-[2px] border-r-[2px] py-[10px] rounded-xl text-[#1cb0f6] font-bold text-[14px] focus:border-b-[2px] lg:md:w-[40%] w-[50%] hover:bg-[#E5E5E5]" type="submit"><AiOutlineGoogle className="text-red-400 text-[25px] mr-[4px]" />Google</button>
+                            </div>
+                            <div className="mt-4 text-center">
+                                <Link href="/terms" className="cursor-pointer font-bold text-center">Terms and condition</Link>
+                                <Link href="/privacy" className="cursor-pointer font-bold text-center ml-3">Privacy policy</Link>
+                            </div>
+                        </div>
                     </div>
-            </div>
+                </div>
                
         </div>
         </>
