0f68627c20f5fd7702e96ddc873e72262191f9e2
al-shaimon <alshaimon968@gmail.com>
2023-01-26 02:03:16 +0600

    editblog completed

--- components/Dashboard/UserComps/EditBlog.js
+++ components/Dashboard/UserComps/EditBlog.js
@@ -9,6 +9,12 @@ const EditBlog = () => {
   const router = useRouter();
   const blogid = router.query.editblogdetail;
 
+  const currentdate = new Date();
+  const newDate = currentdate.toLocaleDateString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
   const {
     _id,
     title,
@@ -46,7 +52,7 @@ const EditBlog = () => {
     // }
   }, [blogid]);
 
-  console.log(blogDetails);
+  // console.log("This is blogDetails",blogDetails);
 
   const {
     register,
@@ -56,8 +62,56 @@ const EditBlog = () => {
   } = useForm();
   const [blogStatus, setBlogStatus] = useState('');
   console.log(blogStatus);
-  const onEditBlog = async (data) => {
-    console.log(data);
+  // const {
+  //   _id,
+  //   title,
+  //   details,
+  //   date,
+  //   author_name,
+  //   author_img,
+  //   image,
+  //   tag,
+  //   package: my_package,
+  // } = blogDetails;
+
+  const onEditBlog = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const details = form.details.value;
+    const tag = form.tag.value;
+    const getPack = form.package.value;
+    let my_package;
+    if (getPack === 'premium') {
+      my_package = '3';
+    }
+
+    const tags = tag.split(',');
+    const editBlogBody = {
+
+      title,
+      details,
+      date: newDate,
+      author_name,
+      author_img,
+      image,
+      tag: tags,
+      package: getPack,
+      gems: my_package,
+    };
+    fetch(`https://hello-talk-webserver.vercel.app/upblog/${_id}`, {
+      method: 'POST',
+      headers: { 'content-Type': 'application/json' },
+      // authorization: `bearer ${localStorage.getItem("s-token")}`,
+      body: JSON.stringify(editBlogBody),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert('Edit blog Successfull');
+        console.log(data);
+      });
+
+    // console.log('This is data', title, details, tag, getPack);
   };
 
   return (
@@ -68,7 +122,7 @@ const EditBlog = () => {
             <h2 className="card-title text-[#333]">Edit Blog</h2>
 
             <div className="shadow-xl py-4 px-[8px]">
-              <form onSubmit={handleSubmit(onEditBlog)}>
+              <form onSubmit={onEditBlog}>
                 <div className="w-full flex justify-between gap-x-[10px]">
                   <div className="w-[100%]">
                     <input
@@ -76,69 +130,75 @@ const EditBlog = () => {
                       placeholder="Blog Title"
                       defaultValue={title}
                       className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                      {...register('courseTitle')}
+                      name="title"
                     />
                     <input
                       type="text"
                       placeholder="Author Name"
                       defaultValue={author_name}
                       className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                      {...register('author')}
+                      name="author_name"
                     />
                     <input
                       type="text"
-                      placeholder="Blog Tags"
+                      placeholder="Use comma(,) for multiple tags"
                       defaultValue={tag}
                       className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                      {...register('tags')}
+                      // {...register('tags')}
+                      name="tag"
                     />
-                    <input
+                    {/* <input
                       type="url"
                       placeholder="Author Image URL"
                       defaultValue={author_img}
                       className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
                       {...register('image')}
-                    />
+                    /> */}
                   </div>
                   <div className="w-[100%]">
                     <textarea
                       className="textarea w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] "
                       placeholder="Blog Content"
                       defaultValue={details}
-                      {...register('courseContent')}
+                      // {...register('courseContent')}
+                      name="details"
                     />
 
                     <select
                       className="select select-bordered w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
                       onChange={(e) => setBlogStatus(e.target.value)}
-                      {...register('status')}
+                      // {...register('status')}
+                      name="package"
                     >
                       <option value="Free">Free</option>
                       <option value="Premium">Premium</option>
                     </select>
-                    {blogStatus === 'Premium' && (
+                    {/* {blogStatus === 'Premium' && (
                       <>
                         <input
                           type="number"
                           placeholder="Price"
                           className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                          {...register('price')}
+                          // {...register('price')}
+                          name="price"
                         />
                         <input
                           type="number"
                           placeholder="Gems"
                           className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
-                          {...register('priceGem')}
+                          // {...register('priceGem')}
+                          name="gems"
                         />
                       </>
-                    )}
-                    <input
+                    )} */}
+
+                    {/* <input
                       type="text"
                       placeholder="Blog Image URL"
                       defaultValue={image}
                       className="input w-full max-w-md bg-[#F7F7F7] border-[2px] border-[#e5e3e3] focus:border-[2px] focus:border-[#e5e3e3] mb-[10px]"
                       {...register('image')}
-                    />
+                    /> */}
                   </div>
                 </div>
                 <button
