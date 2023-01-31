6233e0b42bf5af4ce534f388d333d74d89cfc882
al-shaimon <alshaimon968@gmail.com>
2023-01-31 16:02:31 +0600

    courses bought

---
+++ components/Dashboard/UserComps/CoursesBought.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import useCourses from '../../hooks/useCourses';
+import { FaEdit, FaRegGem, FaTrash } from 'react-icons/fa';
+import Link from 'next/link';
+
+const CoursesBought = () => {
+  const [courses, loading] = useCourses();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  useEffect(() => {
+    if (!loading) {
+      setFilteredCourses(courses);
+    }
+  }, [courses, loading]);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const filteredData = courses.filter((course) =>
+      course.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setFilteredCourses(filteredData);
+  };
+
+  const handleDelete = async (courseId) => {
+    try {
+      await fetch(`https://hello-talk-webserver.vercel.app/course/${courseId}`, {
+        method: 'DELETE',
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+      // update the courses state and re-render the component
+      // ...
+    } catch (error) {
+      console.error(error);
+    } finally {
+      location.reload(true);
+    }
+  };
+
+  return (
+    <div className="md:pt-[5rem] pt-[4.5rem]">
+      <div className="card-body" style={{ width: '100%' }}>
+        <h2 className="card-title text-[#1d4d87]">Course Revenue</h2>
+        <input
+          type="search"
+          placeholder="Type Course Title here"
+          className="input bg-[#d7d5ff]"
+          style={{ width: '50%' }}
+          onChange={handleSearch}
+          value={searchTerm}
+        />
+
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="overflow-x-auto my-2 shadow-2xl rounded">
+            <table className="table-normal" style={{ width: '100%' }}>
+              <thead className="text-center">
+                <tr className="bg-[#1d4d87] text-white">
+                  <th className="text-start">Course Title</th>
+                  <th>Total Sold</th>
+                  <th>Offer Price</th>
+                  <th>Total Revenue</th>
+                </tr>
+              </thead>
+              {filteredCourses.map((course) => {
+                return (
+                  <tbody className="text-left" key={course?._id}>
+                    <tr className="bg-[#d7d5ff] text-[#1d4d87]">
+                      <td className="text-left">{course?.title}</td>
+                      {/* <td>{course?.picture}</td> */}
+                      <td className="text-center">2</td>
+                      <td className="text-center">{course?.offer_price}</td>
+                      <td className="text-center">{2 * course?.offer_price}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CoursesBought;
--- pages/dashboard/coursesbought.js
+++ pages/dashboard/coursesbought.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import DashSidebar from '../../components/Dashboard/DashSidebar';
+import CoursesBought from '../../components/Dashboard/UserComps/CoursesBought';
+import Navbar from '../../components/Shared/Navbar/Navbar';
 
-const coursesbought = () => {
+const coursebought = () => {
   return (
     <div>
-      <h1>Courses bought</h1>
+      <Navbar />
+      <div className="grid  lg:md:grid-cols-[300px_minmax(900px,_1fr)]">
+        <DashSidebar />
+        <CoursesBought/>
+      </div>
     </div>
   );
 };
 
-export default coursesbought;
+export default coursebought;
