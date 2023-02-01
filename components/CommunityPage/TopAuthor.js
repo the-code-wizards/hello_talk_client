5ef08101a53a502b4c4e6ac4864e828cf08efd70
Galib6 <asadullahalgalib81@gmail.com>
2023-02-02 00:16:13 +0600

    top author completed

--- components/CommunityPage/TopAuthor.js
+++ components/CommunityPage/TopAuthor.js
@@ -7,6 +7,23 @@ const TopAuthor = () => {
     const [fetchLoading, setfetchLoading] = useState(false)
     const usersCommentCount = []
     const [sortedUser, setSortedUser] = useState([])
+    const [topAuthors, setTopAuthors] = useState([])
+    const [slicedData, setslicedData] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/topAuthors")
+            .then(res => res.json())
+            .then(data => {
+                if (data.length >= 1) {
+                    setTopAuthors(data)
+                    const sliced = topAuthors.slice(3, topAuthors.length)
+                    setslicedData(sliced)
+                    console.log(slicedData)
+                }
+            })
+    }, [])
+
+
 
     const reloadFetch = () => {
         const fetchPosts = async () => {
@@ -31,13 +48,14 @@ const TopAuthor = () => {
     const userCommentfetch = (users) => {
         users.forEach(function (item, index) {
             // console.log(item)
-            const { email } = item
-            console.log(email)
+            const { email, name } = item
+            // console.log(email)
             fetch(`https://hello-talk-webserver.vercel.app/commentcount?email=${email}`)
                 .then(res => res.json())
                 .then(data => {
                     const userData = {
                         email,
+                        name,
                         totalCommented: data.length
                     }
                     usersCommentCount.push(userData)
@@ -78,8 +96,6 @@ const TopAuthor = () => {
     }
 
 
-
-
     return (
         <div>
             <div className="card w-80 bg-[#fff] shadow-xl">
@@ -93,19 +109,19 @@ const TopAuthor = () => {
                                 </div>
                             </div>
                             <div className=' bg-white mt-2 rounded-lg'>
-                                <h1 className='text-[14px] text-center font-bold '>banhien</h1>
+                                <h1 className='text-[14px] text-center font-bold '>{topAuthors[1]?.name}</h1>
                                 <p className=' text-[14px] text-center font-bold'>#2</p>
                             </div>
 
                         </div>
-                        <div className='mx-3 col-span-4 mt-5'>
+                        <div className=' col-span-4 mt-5'>
                             <div className="avatar  place-self-center">
                                 <div className="w-24 rounded-full ring ring-[#189bd3] ring-offset-base-100 ring-offset-2 mt-4">
                                     <img src="https://placeimg.com/192/192/people" />
                                 </div>
                             </div>
                             <div className=' bg-white mt-2 rounded-lg'>
-                                <h1 className='text-[14px] text-center font-bold'>banhien</h1>
+                                <h1 className='text-[14px] text-center font-bold'>{topAuthors[0]?.name}</h1>
                                 <p className=' text-[14px] text-center font-bold'>#1</p>
                             </div>
                         </div>
@@ -116,81 +132,34 @@ const TopAuthor = () => {
                                 </div>
                             </div>
                             <div className=' bg-white mt-2 rounded-lg'>
-                                <h1 className='text-[14px] text-center font-bold'>banhien</h1>
+                                <h1 className='text-[14px] text-center font-bold'>{topAuthors[2]?.name}</h1>
                                 <p className=' text-[14px] text-center font-bold'>#3</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button className='btn btn-sm' onClick={reloadFetch}>Reload</button>
+                {/* <button className='btn btn-sm' onClick={reloadFetch}>Reload</button> */}
 
-                <div className="card-body">
+                <div className="px-8 pb-4">
                     <div>
-                        <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-3'>
-                            <h1 className='col-span-2 place-self-center'>#4</h1>
-                            <div className='flex col-span-10 items-center'>
-                                <div className="avatar">
-                                    <div className="w-8 rounded-full">
-                                        <img src="https://placeimg.com/192/192/people" />
+                        {
+                            slicedData.map((slice, i) => <>
+                                <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-2'>
+                                    <h1 className='col-span-2 place-self-center'>#{i + 1}</h1>
+                                    <div className='flex col-span-10 items-center'>
+                                        <div className="avatar">
+                                            <div className="w-8 rounded-full">
+                                                <img src="https://placeimg.com/192/192/people" />
+                                            </div>
+                                        </div>
+                                        <h1 className='ml-2'>
+                                            {slice.name}
+                                        </h1>
                                     </div>
                                 </div>
-                                <h1 className='ml-2'>
-                                    Shimul Islam
-                                </h1>
-                            </div>
-                        </div>
-                        <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-3'>
-                            <h1 className='col-span-2 place-self-center'>#4</h1>
-                            <div className='flex col-span-10 items-center'>
-                                <div className="avatar">
-                                    <div className="w-8 rounded-full">
-                                        <img src="https://placeimg.com/192/192/people" />
-                                    </div>
-                                </div>
-                                <h1 className='ml-2'>
-                                    Shimul Islam
-                                </h1>
-                            </div>
-                        </div>
-                        <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-3'>
-                            <h1 className='col-span-2 place-self-center'>#4</h1>
-                            <div className='flex col-span-10 items-center'>
-                                <div className="avatar">
-                                    <div className="w-8 rounded-full">
-                                        <img src="https://placeimg.com/192/192/people" />
-                                    </div>
-                                </div>
-                                <h1 className='ml-2'>
-                                    Shimul Islam
-                                </h1>
-                            </div>
-                        </div>
-                        <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-3'>
-                            <h1 className='col-span-2 place-self-center'>#4</h1>
-                            <div className='flex col-span-10 items-center'>
-                                <div className="avatar">
-                                    <div className="w-8 rounded-full">
-                                        <img src="https://placeimg.com/192/192/people" />
-                                    </div>
-                                </div>
-                                <h1 className='ml-2'>
-                                    Shimul Islam
-                                </h1>
-                            </div>
-                        </div>
-                        <div className='grid grid-cols-12 bg-[#F8EDE3] p-2 rounded-lg mt-3'>
-                            <h1 className='col-span-2 place-self-center'>#4</h1>
-                            <div className='flex col-span-10 items-center'>
-                                <div className="avatar">
-                                    <div className="w-8 rounded-full">
-                                        <img src="https://placeimg.com/192/192/people" />
-                                    </div>
-                                </div>
-                                <h1 className='ml-2'>
-                                    Shimul Islam
-                                </h1>
-                            </div>
-                        </div>
+                            </>)
+                        }
+
                     </div>
                 </div>
             </div>
