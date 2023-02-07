0909d2949da895e78938275e1d1481e529296a0b
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-02-08 00:07:43 +0600

    setup text to speech in teacher's details ,  privacy policy and terms and condition pages

--- pages/ourteacher/[teacherdetails].js
+++ pages/ourteacher/[teacherdetails].js
@@ -1,13 +1,12 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { FaPlayCircle, FaStopCircle } from "react-icons/fa";
 import { useSpeechSynthesis } from "react-speech-kit";
 const Teacherdetails = () => {
   const [teacherDetails, setTeacherDetails] = useState({});
-  const [text, setText] = useState(
-    "Hello! Welcome to my profile. My name is Scott Mendoza and I was born and raised in the United States. I am an experienced university professor and have taught thousands of students all over the world for over 10 years. I teach English for academic purposes, ESL, and academic writing. I have been teaching on Udemy for many years and my courses are some of the highest-ranked and most popular. I do my best to create online English courses that can truly help my students. I provide the best resources and I am always available by message. Please take a look at my courses and let me help you become fluent in English."
-  );
-  const { speak, stop } = useSpeechSynthesis();
-
+  // const [text, setText] = useState(
+  //   "Hello! Welcome to my profile. My name is Scott Mendoza and I was born and raised in the United States. I am an experienced university professor and have taught thousands of students all over the world for over 10 years. I teach English for academic purposes, ESL, and academic writing. I have been teaching on Udemy for many years and my courses are some of the highest-ranked and most popular. I do my best to create online English courses that can truly help my students. I provide the best resources and I am always available by message. Please take a look at my courses and let me help you become fluent in English."
+  // );
   const router = useRouter();
   const teacherid = router.query.teacherdetails;
   const { _id, details, name, image } = teacherDetails;
@@ -19,6 +18,9 @@ const Teacherdetails = () => {
         console.log(data);
       });
   }, [teacherid]);
+  const text =
+    "Hello! Welcome to my profile. My name is Scott Mendoza and I was born and raised in the United States. I am an experienced university professor and have taught thousands of students all over the world for over 10 years. I teach English for academic purposes, ESL, and academic writing. I have been teaching on Udemy for many years and my courses are some of the highest-ranked and most popular. I do my best to create online English courses that can truly help my students. I provide the best resources and I am always available by message. Please take a look at my courses and let me help you become fluent in English.";
+  const { speaking, speak, stop } = useSpeechSynthesis();
   return (
     <div className="card hero pt-[4rem]">
       <div className="card bg-base-100 shadow-xl m-10">
@@ -38,18 +40,26 @@ const Teacherdetails = () => {
         className="block w-full h-32 appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
       /> */}
           {/* <div className="flex items-center mb-6"> */}
-          <button
-            onClick={() => speak({ text })}
+          {/* <button
+            // onClick={() => speak({ text })}
+            
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             Speak
-          </button>
-          <button
+          </button> */}
+          <div>
+            {speaking ? (
+              <FaStopCircle onClick={stop} />
+            ) : (
+              <FaPlayCircle onClick={() => speak({ text })} />
+            )}
+          </div>
+          {/* <button
             onClick={stop}
             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
           >
             Stop
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
