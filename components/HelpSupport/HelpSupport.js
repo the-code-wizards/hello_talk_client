b607632c54410431976cdc467fd5c2aaa3421d53
Sujoy Paul <freelancersujoy@gmail.com>
2023-02-09 16:52:00 +0600

    help and support page

---
+++ components/HelpSupport/HelpSupport.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelpSupport = () => {
+    return (
+        <div className='flex justify-center'>
+            <div>
+            <h2 className='text-2xl'>Support group</h2>
+            <a href="https://meet.google.com/xsr-exra-rxj?pli=1 mt-5" target='_blank' rel="noopener noreferrer"> <button className='btn border-0 bg-green-400'>Join Meet</button> </a>
+            </div>
+        </div>
+    );
+};
+
+export default HelpSupport;
