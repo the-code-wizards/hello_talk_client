123b1aceebdf9da38b55b32f7aeed1890482691d
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-02-11 18:59:53 +0600

    merge

--- pages/terms/index.js
+++ pages/terms/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Link from "next/link";
-<<<<<<< HEAD
-import TermTextToSpeech from "../../components/Terms/TermTextToSpeech";
 import Navbar from "../../components/Shared/Navbar/Navbar";
-=======
->>>>>>> 198130890b3a223305e608b8623154a268da6946
 const index = () => {
   return (
     <section className="mx-36">
