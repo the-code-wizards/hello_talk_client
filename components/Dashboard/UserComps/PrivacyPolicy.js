2f372225702500eb4e113b483a962cfdddd8323b
Sujoy Paul <freelancersujoy@gmail.com>
2023-02-12 12:41:54 +0600

    terms post system added

--- components/Dashboard/UserComps/PrivacyPolicy.js
+++ components/Dashboard/UserComps/PrivacyPolicy.js
@@ -13,17 +13,39 @@ const PrivacyPolicy = () => {
         }
         const confirm = window.confirm('Are you sure want to add privacy?')
         if(confirm){
-            fetch(`http://localhost:5000/addprivacy`, {
+            fetch(`https://hello-talk-webserver.vercel.app/addprivacy`, {
             method: "POST",
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(privacyContent)
+            body: JSON.stringify(privacyBody)
         })
         .then(res => res.json())
         .then(data => {
             if(data.acknowledged){
-                alert('Content added of privacy & policy')
+                alert('content added of privacy & policy')
+            }
+        })
+        }
+    }
+
+    const handleSubmitTerms = () => {
+        const termsBody = {
+            content: termsContent
+        }
+        const confirm = window.confirm('Are you sure want to add terms?')
+        if(confirm){
+            fetch(`https://hello-talk-webserver.vercel.app/addterms`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(termsBody)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged){
+                alert('content added of terms & condition')
             }
         })
         }
@@ -56,6 +78,11 @@ const PrivacyPolicy = () => {
                     onChange={newContent => setTermsContent(newContent)}
                 ></JoditEditor>
             </div>
+
+            <div className='flex justify-end mt-2'>
+                <button onClick={handleSubmitTerms} className='text-[#1A4982] hover:bg-[#dcecff] uppercase px-6 py-2 rounded-md bg-white'>Submit</button>
+            </div>
+
             </div>
         </div>
     );
