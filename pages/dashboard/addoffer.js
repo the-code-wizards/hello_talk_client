386e5d923df1acfdefeac618569fa9c99bf7ff98
al-shaimon <alshaimon968@gmail.com>
2023-02-14 21:04:04 +0600

    private route at dashboard

--- pages/dashboard/addoffer.js
+++ pages/dashboard/addoffer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import DashSidebar from '../../components/Dashboard/DashSidebar';
 import AddOffer from '../../components/Dashboard/UserComps/AddOffer';
+import PrivateRoute from '../../components/hooks/privateRoute';
 import Navbar from '../../components/Shared/Navbar/Navbar';
 
 const index = () => {
-    return (
-        <div>
-            <Navbar />
-            <div className='grid  lg:md:grid-cols-[300px_minmax(900px,_1fr)]'>
-                <DashSidebar />
-                <AddOffer/>
-            </div>
+  return (
+    <div>
+      <PrivateRoute>
+        <Navbar />
+        <div className="grid  lg:md:grid-cols-[300px_minmax(900px,_1fr)]">
+          <DashSidebar />
+          <AddOffer />
         </div>
-    );
+      </PrivateRoute>
+    </div>
+  );
 };
 
 export default index;
--- pages/dashboard/general.js
+++ pages/dashboard/general.js
@@ -2,23 +2,26 @@ import Head from 'next/head';
 import React from 'react';
 import DashSidebar from '../../components/Dashboard/DashSidebar';
 import PrivacyPolicy from '../../components/Dashboard/UserComps/PrivacyPolicy';
+import PrivateRoute from '../../components/hooks/privateRoute';
 import Footer from '../../components/Shared/Footer/Footer';
 import Navbar from '../../components/Shared/Navbar/Navbar';
 
 const index = () => {
   return (
     <div>
-      <div>
-        <Head>
-          <title>HelloTalk - General</title>
-        </Head>
-        <Navbar />
-        <div className="grid lg:md:grid-cols-[300px_minmax(900px,_1fr)]">
-          <DashSidebar />
-          <PrivacyPolicy></PrivacyPolicy>
-          {/* <AllBlogs /> */}
+      <PrivateRoute>
+        <div>
+          <Head>
+            <title>HelloTalk - General</title>
+          </Head>
+          <Navbar />
+          <div className="grid lg:md:grid-cols-[300px_minmax(900px,_1fr)]">
+            <DashSidebar />
+            <PrivacyPolicy></PrivacyPolicy>
+            {/* <AllBlogs /> */}
+          </div>
         </div>
-      </div>
+      </PrivateRoute>
     </div>
   );
 };
