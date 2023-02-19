9fa70b509613a508207b5d121149989c4c4680a5
Sujoy Paul <freelancersujoy@gmail.com>
2023-02-19 20:33:12 +0600

    add teacher problem fix of my profile button

--- pages/dashboard/applyteacher.js
+++ pages/dashboard/applyteacher.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import DashSidebar from '../../components/Dashboard/DashSidebar';
 import ApplyTeacher from '../../components/Dashboard/UserComps/ApplyTeacher';
@@ -6,11 +7,16 @@ import Navbar from '../../components/Shared/Navbar/Navbar';
 const applyteacher = () => {
     return (
         <div>
-            <Navbar />
-            <div className='grid  lg:md:grid-cols-[300px_minmax(900px,_1fr)]'>
-                <DashSidebar />
-                <ApplyTeacher></ApplyTeacher>
-            </div>
+            <div>
+        <Head>
+            <title>Add Blog - HelloTalk</title>
+        </Head>
+          <Navbar />
+          <div className="grid  lg:md:grid-cols-[300px_minmax(900px,_1fr)]">
+            <DashSidebar />
+            <ApplyTeacher />
+          </div>
+      </div>
         </div>
     );
 };
