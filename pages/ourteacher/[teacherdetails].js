0f228e0219902d9110c28f8b295f4f449cfd5778
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-02-22 17:23:11 +0600

    add nabar on  terms and privacy pages

--- pages/ourteacher/[teacherdetails].js
+++ pages/ourteacher/[teacherdetails].js
@@ -6,10 +6,11 @@ import Navbar from "../../components/Shared/Navbar/Navbar";
 // import { useSpeechSynthesis } from "react-speech-kit";
 const Teacherdetails = () => {
   const [teacherDetails, setTeacherDetails] = useState({});
- 
+
   const router = useRouter();
   const teacherid = router.query.teacherdetails;
-  const { _id, details, name, image , title, qualification, email} = teacherDetails;
+  const { _id, details, name, image, title, qualification, email } =
+    teacherDetails;
   useEffect(() => {
     fetch(`https://hello-talk-webserver.vercel.app/teacher/${teacherid}`)
       .then((res) => res.json())
@@ -23,24 +24,33 @@ const Teacherdetails = () => {
   // const { speaking, speak, stop } = useSpeechSynthesis();
   return (
     <div>
-        <Head>
+      <Head>
         <title> {title} - HelloTalk</title>
       </Head>
       <Navbar></Navbar>
-<div className="pt-32">
-<div className="text-gray-800 md:max-w-[1240px] mx-auto shadow-md rounded-lg">
-	<div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row p-5">
-		<img src={image} alt="" className=" rounded-lg flex-shrink-0 h-[300px] w-[300px] border  md:justify-self-start bg-gray-500 border-gray-300" />
-		<div className="flex flex-col">
-			<h4 className="text-2xl  font-featherBold text-center md:text-left mb-2">{name}</h4>
-			<h4 className="text-md font-featherBold text-center md:text-left mb-4">{qualification}</h4>
-			<p className="text-gray-600">{details}</p>
-			<p className="text-gray-600 font-semibold mt-2">Email: {email?email:"no email found"}</p>
-		</div>
-	</div>
-	
-</div>
-</div>
+      <div className="pt-32">
+        <div className="text-gray-800 md:max-w-[1240px] mx-auto shadow-md rounded-lg">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row p-5">
+            <img
+              src={image}
+              alt=""
+              className=" rounded-lg flex-shrink-0 h-[300px] w-[300px] border  md:justify-self-start bg-gray-500 border-gray-300"
+            />
+            <div className="flex flex-col">
+              <h4 className="text-2xl  font-featherBold text-center md:text-left mb-2">
+                {name}
+              </h4>
+              <h4 className="text-md font-featherBold text-center md:text-left mb-4">
+                {qualification}
+              </h4>
+              <p className="text-gray-600">{details}</p>
+              <p className="text-gray-600 font-semibold mt-2">
+                Email: {email ? email : "no email found"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* <div className="card hero pt-[4rem]">
       <div className="card bg-base-100 shadow-xl m-10">
         <div className="avatar justify-center mt-10">
@@ -52,15 +62,12 @@ const Teacherdetails = () => {
           <h2 className="card-title">{name}</h2>
           <p>{details}</p>
         </div>
-<<<<<<< HEAD
         <div className="flex items-center py-2 mx-10 lg:md:text-[18px] text-[16px]">
           <Link href={`/ourteachers`}>
             <FaArrowLeft></FaArrowLeft>
           </Link>
           <h3 className="px-3">Go To Back</h3>
         </div>
-=======
->>>>>>> 548e253fb920f4af281c357107166715ed0e6107
         <div className="p-6">
          
         </div>
