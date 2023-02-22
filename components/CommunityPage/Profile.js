bc9f4f352837d7914f97375f18efa761bf61f8c9
Galib6 <asadullahalgalib81@gmail.com>
2023-02-22 18:49:12 +0600

    home page payment fixed

--- components/CommunityPage/Profile.js
+++ components/CommunityPage/Profile.js
@@ -45,7 +45,7 @@ const Profile = () => {
                                 <div className='flex flex-col justify-center items-center mt-1'>
                                     <p className='text-center badge badge-outline'>@{singleUser.name}9</p>
                                     <div className='mt-4 flex'>
-                                        <Link href="/messages" className='mx-2 flex justify-center items-center  border rounded-lg px-2 py-1 text-white bg-[#046892] hover:bg-white hover:text-[#046892] hover:border-2 hover:border-[#046892]'>
+                                        <Link href="/messages" className='mx-2 flex justify-center items-center  border rounded-lg px-2 py-1 text-white bg-[#046892] hover:bg-white hover:text-[#046892] hover:border-2 hover:border-[#046892] '>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16"> <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" /> </svg>
                                             <span className='ml-1'>Messages</span>
                                         </Link>
